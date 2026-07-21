01f39e3a9f957cdc906f0cedd5e711cf88a6a952
yashvanth-kumar <vk2260993@gmail.com>
2026-07-21 19:55:15 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,8 +4,12 @@ export function cn(...inputs: ClassValue[]) {
   return clsx(inputs);
 }
 
-export function formatCurrency(amount: number, currencySymbol = "$") {
-  return `${currencySymbol}${amount.toFixed(2)}`;
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    maximumFractionDigits: 2,
+  }).format(amount);
 }
 
 export function slugify(text: string) {
